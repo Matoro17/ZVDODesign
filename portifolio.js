2794e4f177244c38906e1e99a0141770d0f4d7a1
Gabriel <gabrielsilvadeazevedo@gmail.com>
2019-04-11 13:34:16 -0300

    Now the portifolio its scalable

---
+++ portifolio.js
@@ -0,0 +1,121 @@
+
+
+
+data =[
+    {  //Montagem
+        "Titulo": "Montagem",
+        "Subtitulo":"Edição de imagem",
+        "SubSubTitulo": "Edição de imagem",
+        "SubSubSubTitulo": "Edição",
+
+        "Tipo": "Chibata",
+        "Midia": "img/portfolio/01-thumbnail.jpg",
+        "InsideMidia": '<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">        <div class="carousel-inner">          <div class="carousel-item active">            <img class="d-block w-100" src="img/portfolio/01-full-3.jpg" alt="Dino">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/01-full-2.jpg" alt="Castelo flutuante">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/01-full-1.jpg" alt="Basketball">          </div>        </div>        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">          <span class="carousel-control-prev-icon" aria-hidden="true"></span>          <span class="sr-only">Anterior</span>        </a>        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">          <span class="carousel-control-next-icon" aria-hidden="true"></span>          <span class="sr-only">Próximo</span>        </a>      </div>',
+
+        "Descricao": "Montagens de imagem com diversos propositos, artisticos ou práticos",
+        "Data":"2017"
+
+    },
+    {   //Modelagem 3d
+        "Titulo": "Modelagem 3D",
+        "Subtitulo":"Design de ambientes",
+        "SubSubTitulo": "Modelagem de ambientes",
+        "SubSubSubTitulo": "Mostragem de lugares conforme planta e design",
+
+        "Tipo": "Modelagem 3D",
+        "Midia": "img/portfolio/02-thumbnail.jpg",
+        "InsideMidia": '<div id="ambientes" class="carousel slide" data-ride="carousel">        <div class="carousel-inner">          <div class="carousel-item active">            <img class="d-block w-100" src="img/portfolio/02-full-1.jpg" alt="Sala">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/02-full-2.jpg" alt="escritorio">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/02-full-3.jpg" alt="lugar desolado">          </div>        </div>        <a class="carousel-control-prev" href="#ambientes" role="button" data-slide="prev">          <span class="carousel-control-prev-icon" aria-hidden="true"></span>          <span class="sr-only">Anterior</span>        </a>        <a class="carousel-control-next" href="#ambientes" role="button" data-slide="next">          <span class="carousel-control-next-icon" aria-hidden="true"></span>          <span class="sr-only">Próximo</span>        </a>      </div>',
+
+        "Descricao": "Modelagem de ambientes, com base em planta baixa ou não, tenha noção de de um ambiente a ser construido usando modelagem 3D",
+        "Data":"2017"
+        
+    },
+    {//Arduino
+        "Titulo": "Jogos com Arduino",
+        "Subtitulo":"cliente arduino jogos multiplataforma",
+        "SubSubTitulo": "Cliente de Jogos Arduino",
+        "SubSubSubTitulo":"Interface para jogos multiplataforma de Quiz",
+
+        "Tipo": "Programação e Embarcado",
+        "Midia": "img/portfolio/03-thumbnail.jpg",
+        "InsideMidia": '<div id="arduino" class="carousel slide" data-ride="carousel">        <div class="carousel-inner">          <div class="carousel-item active">            <img class="d-block w-100" src="img/portfolio/03-full-1.jpg" alt="">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/03-full-2.jpg" alt="">          </div>        </div>        <a class="carousel-control-prev" href="#arduino" role="button" data-slide="prev">          <span class="carousel-control-prev-icon" aria-hidden="true"></span>          <span class="sr-only">Anterior</span>        </a>        <a class="carousel-control-next" href="#arduino" role="button" data-slide="next">          <span class="carousel-control-next-icon" aria-hidden="true"></span>          <span class="sr-only">Próximo</span>        </a>      </div>', 
+
+        "Descricao": "Numa junção entre programação em C, para o microcontrolador, e um codigo Javascript para adaptar diversos jogos de quiz para varias plataformas, no caso para arduino",
+        "Data":"2018" 
+    },
+    {//
+        "Titulo": "Objetos em 3D",
+        "Subtitulo":"Modelagem de peças e artefatos",
+        "SubSubTitulo": "Objetos 3D",
+        "SubSubSubTitulo":"Modelagem de diversos itens e pequenas cenas em 3D",
+
+        "Tipo": "Modelagem 3D",
+        "Midia": "img/portfolio/04-thumbnail.jpg",
+        "InsideMidia": '<div id="3dobjects" class="carousel slide" data-ride="carousel">        <div class="carousel-inner">          <div class="carousel-item active">            <img class="d-block w-100" src="img/portfolio/04-full-1.jpg" alt="">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/04-full-2.jpg" alt="">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/04-full-3.jpg" alt="">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/04-full-4.jpg" alt="">          </div>        </div>        <a class="carousel-control-prev" href="#3dobjects" role="button" data-slide="prev">          <span class="carousel-control-prev-icon" aria-hidden="true"></span>          <span class="sr-only">Anterior</span>        </a>        <a class="carousel-control-next" href="#3dobjects" role="button" data-slide="next">          <span class="carousel-control-next-icon" aria-hidden="true"></span>          <span class="sr-only">Próximo</span>        </a>      </div>', 
+
+        "Descricao": "Modelagem de cenas e Objetos em 3D, podendo ser brinquedos, cenas, protótipos de produtos ou peças",
+        "Data":"2017" 
+    },
+    {//
+        "Titulo": "Design Gráfico",
+        "Subtitulo":"Folders e cartões de visita",
+        "SubSubTitulo": "Design grafico",
+        "SubSubSubTitulo":"Serviços de design gráfico",
+
+        "Tipo": "Design gráfico",
+        "Midia": "img/portfolio/05-thumbnail.jpg",
+        "InsideMidia": '<div id="grafico" class="carousel slide" data-ride="carousel">        <div class="carousel-inner">          <div class="carousel-item active">            <img class="d-block w-100" src="img/portfolio/05-full-1.jpg" alt="">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/05-full-2.jpg" alt="">          </div>        </div>        <a class="carousel-control-prev" href="#grafico" role="button" data-slide="prev">          <span class="carousel-control-prev-icon" aria-hidden="true"></span>          <span class="sr-only">Anterior</span>        </a>        <a class="carousel-control-next" href="#grafico" role="button" data-slide="next">          <span class="carousel-control-next-icon" aria-hidden="true"></span>          <span class="sr-only">Próximo</span>        </a>      </div>', 
+
+        "Descricao": "Serviços de criação de logos, carões de visita, folders, panfletos, e diversos tipos de marketing digital e impresso",
+        "Data":"2018" 
+    },
+    {//
+        "Titulo": "Projeção de arquitetura",
+        "Subtitulo":"Projeto para Rua Sales Barbosa",
+        "SubSubTitulo": "Projeto de arquitetura externa",
+        "SubSubSubTitulo":"Projeção da rua sales barbosa com um lugar de recreação",
+
+        "Tipo": "Modelagem 3D",
+        "Midia": "img/portfolio/06-thumbnail.jpg",
+        "InsideMidia": '<div id="sales" class="carousel slide" data-ride="carousel">        <div class="carousel-inner">          <div class="carousel-item active">            <img class="d-block w-100" src="img/portfolio/06-full-1.jpg" alt="">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/06-full-2.jpg" alt="">          </div>          <div class="carousel-item">            <img class="d-block w-100" src="img/portfolio/06-full-3.jpg" alt="">          </div>        </div>        <a class="carousel-control-prev" href="#sales" role="button" data-slide="prev">          <span class="carousel-control-prev-icon" aria-hidden="true"></span>          <span class="sr-only">Anterior</span>        </a>        <a class="carousel-control-next" href="#sales" role="button" data-slide="next">          <span class="carousel-control-next-icon" aria-hidden="true"></span>          <span class="sr-only">Próximo</span>        </a>      </div>', 
+
+        "Descricao": "Projeto visando transformar a rua sales barbosa em um lugar mais atrativo turisticamente. Modelagem a base de planta baixa num total de 1,5 km, sendo possível saber o resultato a partir da modelagem 3D",
+        "Data":"2017" 
+    },
+    {//
+        "Titulo": "Vídeo FX",
+        "Subtitulo":"Edição e efeitos especiais de vídeo",
+        "SubSubTitulo": "Edição e efeitos de vídeo",
+        "SubSubSubTitulo":"Projeto de propaganda de celular",
+
+        "Tipo": "Vídeo",
+        "Midia": "img/portfolio/07-thumbnail.jpg",
+        "InsideMidia": '<iframe width="560" height="315" src="https://www.youtube.com/embed/G0auga6Bu2s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', 
+
+        "Descricao": "Vídeo promocional de celular fictício para demonstração de uso de diversos efeitos especiais em vídeo",
+        "Data":"2017" 
+    },
+    {//
+        "Titulo": "Efeito Holograma",
+        "Subtitulo":"Video FX de efeito holograma",
+        "SubSubTitulo": "Projeto de holograma",
+        "SubSubSubTitulo":"efeito de holograma",
+
+        "Tipo": "Vídeo",
+        "Midia": "img/portfolio/08-thumbnail.jpg",
+        "InsideMidia": '<iframe width="560" height="315" src="https://www.youtube.com/embed/gw1f8eFq2DA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', 
+
+        "Descricao": "Demonstração de Efeitos e motion tracking para executa uma holograma",
+        "Data":"2017" 
+    }
+]
+
+for(var i = 0 ; i<data.length;i++){
+    document.getElementById("loader").innerHTML +=  '<div class="col-md-4 col-sm-6 portfolio-item"><a class="portfolio-link" data-toggle="modal"href="#portfolioModal'+i+'"><div class="portfolio-hover"><div class="portfolio-hover-content"><i class="fa fa-plus fa-3x"></i></div></div><img class="img-fluid" src='+data[i].Midia+' alt=""></a><div class="portfolio-caption"><h4>'+data[i].Titulo+'</h4><p class="text-muted">'+data[i].Subtitulo+'</p></div></div>';
+
+    document.getElementById("secondLoader").innerHTML += '<div class="portfolio-modal modal fade" id="portfolioModal'+i+'" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content">  <div class="close-modal" data-dismiss="modal">    <div class="lr">      <div class="rl"></div>    </div>  </div>  <div class="container">    <div class="row">      <div class="col-lg-8 mx-auto">        <div class="modal-body">                    <h2 class="text-uppercase">'+data[i].SubSubTitulo+'</h2>          <p class="item-intro text-muted">'+data[i].SubSubSubTitulo+'</p>          '+data[i].InsideMidia+'<p>'+data[i].Descricao+'</p>          <ul class="list-inline">            <li>Data: '+data[i].Data+'</li>            <li>Categoria: '+data[i].Subtitulo+'</li>          </ul>          <button class="btn btn-primary" data-dismiss="modal" type="button">            <i class="fa fa-times"></i>             Fechar Projeto </button>        </div>      </div>    </div>  </div></div></div></div>';
+
+}
+
+
+
